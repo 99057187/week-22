a8041ab62ebecb44afda71ab5c9d855b630da97c
anthony <99057187@mydavinci.nl>
2020-02-18 09:28:01 +0100

    no message

---
+++ welcome.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="welcome.css">
+		<title>welcome</title>
+	</head>
+	<body>
+		<label for="lname">naam:</label><br>
+  		<input type="text" id="naam"value=""><br><br>
+		<label for="lname">e-mail:</label><br>
+  		<input type="text" id="e-mail"value=""><br><br>    
+	</body>
+</html>
