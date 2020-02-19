f0dc9337f41af567335e730f9ce6fb915b49e013
anthony <99057187@mydavinci.nl>
2020-02-19 09:05:49 +0100

    no message

--- welcome.php
+++ welcome.php
@@ -8,9 +8,11 @@
 		<title>welcome</title>
 	</head>
 	<body>
-		<label for="lname">naam:</label><br>
-  		<input type="text" id="naam"value=""><br><br>
-		<label for="lname">e-mail:</label><br>
-  		<input type="text" id="e-mail"value=""><br><br>    
+	<form action="" method="post">
+			<label for="gebruikersnaam" id="naamTekst">naam:</label><br>
+  			<input type="text" id="naam" name="naam" value=""><br><br>
+			<label for="e-mail" id="e-mailTekst">e-mail:</label><br>
+  			<input type="e-mail" id="e-mail" name="e-mail" value=""><br><br> 
+  			<input type="submit"> 
 	</body>
 </html>
